89cc0496ecd83957d0c71d0750ffd141fad5b86c
Alexandr-sys2024 <time2016slot@gmail.com>
2024-12-15 23:38:16 +0300

    add new code in bot

--- financial_bot.py
+++ financial_bot.py
@@ -77,6 +77,27 @@ async def registration(message: Message):
        conn.commit()
        await message.answer("Вы успешно зарегистрированы!")
 
+#Команда курса валют
+@dp.message(F.text == "Курс валют")
+async def exchange_rates(message: Message):
+   url = "https://v6.exchangerate-api.com/v6/09edf8b2bb246e1f801cbfba/latest/USD"
+   try:
+       response = requests.get(url)
+       data = response.json()
+       if response.status_code != 200:
+           await message.answer("Не удалось получить данные о курсе валют!")
+           return
+       usd_to_rub = data['conversion_rates']['RUB']
+       eur_to_usd = data['conversion_rates']['EUR']
+
+       euro_to_rub = eur_to_usd * usd_to_rub
+
+       await message.answer(f"1 USD - {usd_to_rub:.2f}  RUB\n"
+                            f"1 EUR - {euro_to_rub:.2f}  RUB")
+
+
+   except:
+       await message.answer("Произошла ошибка")
 
 
 
