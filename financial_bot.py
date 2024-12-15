e0afc4f2065dc461f0f9be6528ddba9d00b7a062
Alexandr-sys2024 <time2016slot@gmail.com>
2024-12-15 23:22:21 +0300

    add new code in bot

--- financial_bot.py
+++ financial_bot.py
@@ -50,6 +50,14 @@ CREATE TABLE IF NOT EXISTS users (
 
 conn.commit()
 
+#Создание класса состояния групп
+class FinancesForm(StatesGroup):
+   category1 = State()
+   expenses1 = State()
+   category2 = State()
+   expenses2 = State()
+   category3 = State()
+   expenses3 = State()
 
 
 
